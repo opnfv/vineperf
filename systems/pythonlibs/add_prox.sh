c3732972780234f38799616d1efef3a19926537a
Sridhar K. N. Rao <sridhar.rao@spirent.com>
2021-08-04 17:10:27 +0530

    Integrating DPPD-Prox with Vineperf
    This patch adds Integration of DPPD-Prox with Vineperf
    Added:
    1. Results analysis - bug-fix
    2. Restrict to baremetal for this patch.
    3. Fix Pylint errors.
    4. Copy Rapid private key too.
    5. Remove SSH key.
    
    Signed-off-by: Sridhar K. N. Rao <sridhar.rao@spirent.com>
    Change-Id: I3ea7e6261e3fab06ef829f22153fe247372fc34b

---
+++ systems/pythonlibs/add_prox.sh
@@ -0,0 +1,14 @@
+VINEPERFENV_DIR="$HOME/vsperfenv"
+
+if [ ! -d "$VINEPERFENV_DIR" ] ; then
+    echo "Directory $VINEPERFENV_DIR should exists. Please complete normal installation process"
+    exit
+fi
+
+(source "$VINEPERFENV_DIR"/bin/activate
+git clone https://gerrit.opnfv.org/gerrit/samplevnf /tmp/samplevnf
+cd /tmp/samplevnf/VNFs/DPPD-PROX/helper-scripts/rapid
+python setup.py build
+python setup.py install
+cp rapid_rsa_key ~/.ssh/
+rm -rf /tmp/samplevnf)
